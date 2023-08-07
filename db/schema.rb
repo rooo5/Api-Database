b58791a92f26505631d9c5787c0db387b192d14b
rooo5 <rg96200480@gmail.com>
2023-08-07 11:53:25 +0530

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_25_064933) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_122949) do
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "typeaheads", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
